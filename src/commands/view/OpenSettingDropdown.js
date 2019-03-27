b16f83b306630689c9dc01813df122c06c7e1cf6
freddvch <freddvch@gmail.com>
2019-03-27 20:10:17 +0300

    the change caret function on open setting dropdown command was added

--- src/commands/view/OpenSettingDropdown.js
+++ src/commands/view/OpenSettingDropdown.js
@@ -2,8 +2,11 @@ module.exports = {
   run(editor, sender) {
     const elements = $('.gjs-sm-sectors .gjs-sm-sector .gjs-sm-properties');
     const sector = $('.gjs-sm-sectors .gjs-sm-sector');
+    const caret = $('.fa-caret-right');
     $(elements).show();
     $(sector).addClass('gjs-sm-open');
+    $(caret).addClass('fa-caret-down');
+    $(caret).removeClass('fa-caret-right');
   },
 
   stop() {
