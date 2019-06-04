098ba9771c5c926f9c7e337a9c49d6fc32ecec4a
Juan <jcamejo3@gmail.com>
2019-06-04 17:46:19 +0200

    adding a small script that get html page from and server , renders on the editor and saves the editor save object

--- src/editor/config/config.js
+++ src/editor/config/config.js
@@ -70,7 +70,7 @@ module.exports = {
   `,
 
   // CSS that could only be seen (for instance, inside the code viewer)
-  protectedCss: '* { box-sizing: border-box; } body {margin: 0;}',
+  protectedCss: '',
 
   // CSS for the iframe which containing the canvas, useful if you need to custom something inside
   // (eg. the style of the selected component)
